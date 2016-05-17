a94f0dc50c60e94ec7c738706eaa53f976f47704
cloudZQY <498219667@qq.com>
2016-05-17 22:00:45 +0800

    样式，复制增加

--- javascripts/app.js
+++ javascripts/app.js
@@ -31,7 +31,32 @@ $(function(){
 				},
 				success: function(data){
 					data.forEach(function(gal,index){
-						var $imgD = $('<div class="box"><img src="'+ gal.img +'"></div>');
+						var html = '<div class="box">'+
+										'<div class="imgBox" data-clipboard-text="'+ gal.torrent +' title="Click to copy to clipboard.">'+
+											'<img src="'+ gal.img +'">'+ (
+												gal.torrent?
+													'<div class="shadow "><p>点击复制磁力链</p></div>':
+													'<div class="shadow "><p>无资源</p></div>'
+													) +
+										'</div>'+
+										'<div class="content">'+
+											'<ul>'+
+												'<li>'+
+													'<p class="title">游戏名</p>'+
+													'<p class="ellipsis">'+ gal.title +'</p>'+
+												'</li>'+
+												'<li>'+
+													'<p class="title">发售日期</p>'+
+													'<p class="ellipsis">'+ gal.date +'</p>'+
+												'</li>'+
+												'<li>'+
+													'<p class="title">大小</p>'+
+													'<p class="ellipsis">'+ gal.size +' G</p>'+
+												'</li>'+
+											'</ul>'+
+										'</div>'+
+									'</div>';
+						var $imgD = $(html);
 						$imgD.find('img').on('load',function(){
 							if(index > _this.num-4) {
 								_this.busy = false;
@@ -56,7 +81,26 @@ $(function(){
 			   if($(this).height() - $(window).height() - $(this).scrollTop() <10 && !_this.busy){ 
 			   		_this.getGals();
 			   }  
-			});  
+			});
+			
+			$('#masonry').on('mouseover','.imgBox',function(){
+				var client = new ZeroClipboard(this);
+				client.on( "copy", function( event ) {
+					t = setTimeout(function(){
+						clearTimeout(t);
+						alert('复制成功',event.target);
+					},100);	
+				});
+			});
+			window.alert = function(msg,el){
+				$(el).parent().append('<p class="alert">'+ msg +'</p>');
+				setTimeout(function(){
+					$('.alert').fadeOut('slow',function(){
+						$(this).remove();
+					});
+				},800)
+			};
+			
 		}
 	}
 	gals.init();
